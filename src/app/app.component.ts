080520e33cabceb5b69310fdd14e287b994a9e67
hoilun <2167686@cegepmontpetit.ca>
2023-01-24 15:58:03 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Lab1';
+  //title = 'Lab1';
+  n = number;
+  cheapWisdom = "LOL!";
+  hateList:Array<string> = ["Something", "Something Hate more", "Something hatre even more"];
 }
+let NothateList: Array<string> = ["Doggo1", "Doggo2", "Doggo3"]
+const number = 13
